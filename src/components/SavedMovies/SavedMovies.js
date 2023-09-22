4e318c3df53ada302672a4a03a36a2ad2edc957f
Mariia Ionova <ionova_1999@list.ru>
2023-09-22 17:43:07 +0300

    создать страницы фильмов и папку build

---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,24 @@
+import "./SavedMovies.css";
+import SearchForm from "../SearchForm/SearchForm";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+function SavedMovies({ isLoggedIn, onClickBurger, isBurgerOpened }) {
+  return (
+    <>
+      <Header
+        isLoggedIn={isLoggedIn}
+        onClickBurger={onClickBurger}
+        isBurgerOpened={isBurgerOpened}
+      />
+      <main className="saved-movies">
+        <SearchForm />
+        <MoviesCardList />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default SavedMovies;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,32 @@
+import "./SearchForm.css";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+
+function SearchForm() {
+  return (
+    <section className="search">
+      <form className="search__form" name="search-form" noValidate>
+        <div className="search__container">
+          <label className="search__label">
+            <input
+              className="search__input"
+              name="search-input"
+              id="search-input"
+              type="text"
+              placeholder="Фильм"
+              value=""
+            />
+            <span className="search__error"></span>
+          </label>
+          <button
+            type="submit"
+            className="search__button"
+            aria-label="Найти фильмы."
+          ></button>
+        </div>
+        <FilterCheckbox />
+      </form>
+    </section>
+  );
+}
+
+export default SearchForm;
